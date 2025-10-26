a58a1b586c86d2454f6ad0a398cde2343dcfeb68
Sunny <79506911+ItsSunnyyy@users.noreply.github.com>
2025-10-26 09:30:31 -0400

    Update volcano.user.js

--- volcano.user.js
+++ volcano.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Auto Bypass Volcano
+// @name         UtilityHub Bypass Volcano
 // @namespace    http://tampermonkey.net/
 // @version      1.6.2.2
 // @description  You only need to solve the captcha
-// @author       DyRian (based on IHaxU)
+// @author       UtilityHub
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @run-at       document-start
 // @grant        unsafeWindow
-// @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
+// @icon         https://i.postimg.cc/3RYBKrFz/UH-Official.png
 // @downloadURL  https://github.com/dyr14n/bypass/raw/main/volcano.user.js
 // @updateURL    https://github.com/dyr14n/bypass/raw/main/volcano.user.js
 // @require      https://github.com/dyr14n/bypass/raw/main/bypassvolcano.js
